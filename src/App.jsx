9e1897947bc565d34b7c24f950fb8ed328a23988
Dmutro Zelenyi <156704336+DmutroZel@users.noreply.github.com>
2025-08-07 18:02:25 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import TypingTrainer from './components/TypingTrainer/TypingTrainer';
+
+export default function App() {
+  return (
+    <div>
+      <TypingTrainer />
+    </div>
+  )
+}
